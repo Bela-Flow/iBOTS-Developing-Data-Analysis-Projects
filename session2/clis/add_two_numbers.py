1a64596b495b2f1c7e0dd270c4245005c5cc0fc7
Bela Erlinghagen <bela@erlinghagen.org>
2024-02-26 15:48:48 +0100

    changes add + transform

--- session2/clis/add_two_numbers.py
+++ session2/clis/add_two_numbers.py
@@ -6,10 +6,11 @@ parser = argparse.ArgumentParser(description='Add2Numbers')
 # Add an argument
 parser.add_argument('number1', type=int, help='First number to add')
 parser.add_argument('number2', type=int, help='Second number to add')
+parser.add_argument('--scale', type=int, default=1)
 
 args = parser.parse_args()
 
-result = args.number1 + args.number2
+result = (args.number1 + args.number2) * args.scale
 
 print(f"The result is: {result}")
 
