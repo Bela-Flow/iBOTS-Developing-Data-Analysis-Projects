4b87923d5550137aa523e2a3d45bbfca151fe45f
Bela Erlinghagen <bela@erlinghagen.org>
2024-02-27 11:20:23 +0100

    Finished FunctionsfromModulesNotebook

--- session3/src/stats.py
+++ session3/src/stats.py
@@ -18,10 +18,6 @@ def calculate_median(numbers):
         median = numbers[mid]
     return median
 
-def plot_histogram(data, nbins = 20, color = "black"):
-    figure, ax = plt.subplots(ncols=1, nrows=1)
-    ax.hist(data, bins=nbins, color = color);
-
 def compute_stat(data, kind=calculate_mean):
     return kind(data)
 
